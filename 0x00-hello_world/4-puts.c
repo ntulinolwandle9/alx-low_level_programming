d8dd191ac31667b1615200b2b725136ae4d27342
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-09 08:26:58 -0700

    learning c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
- int main(void)
+/**
+ * main-Printing
+ *
+ * Return: Always 0(successful)
+ */
+int main(void)
 { 
-	puts("\"Progarmming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 } 
