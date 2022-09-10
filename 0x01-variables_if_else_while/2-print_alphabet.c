c1025b5d7fc3b8083030a524b59c2b13e757ed60
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-09 20:30:03 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+/**
+ * main - lowercase alphabets
+ *
+ * Return : Alwyas (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar("\n");
+	return(0);
+}
