a004a226a7907d6c38dd93d2c78fa88294af2e6b
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-15 20:26:36 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers to 14
+ *
+ * Return: returns nothing
+ */
+void more_numbers(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 18; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			if (num2 > 9)
+			{
+				putchar((num2 / 10) + '0');
+			}
+			putchar((num2 % 10) + '0');
+		}
+		putchar(10);
+	}
+}
+
