f73244059c603a99af999f380f6a890a09c7962b
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-19 14:02:54 -0700

    printing strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,16 @@
 
 /**
  * _strlen - returns the length of a string
- * @s - parameter
- * Return - length of a string
+ * @s: parameter
+ * Return: length of a string
  */
 int _strlen(char *s)
 {
-	return _strlen(s);
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+	return (length);
 }
