62649e412f03505c7343a0470ad20812926256d4
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-19 20:41:55 -0700

    copy and paste string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "mainn.h"
+
+/**
+ * _strcpy - Copies and pastes string
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
