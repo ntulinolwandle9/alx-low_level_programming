5b067fa18c6d189f42da4227b385e20dc86984ab
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-22 19:40:26 -0700

    capitalising a string

---
+++ 0x06-pointers_arrays_strings/6-cao_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizees all words of a string
+ * @str: parameter to be capitalized
+ *
+ * Return: pointer to a string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
+		if (
+	}
+	
+}
