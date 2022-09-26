7eb5024fcd4f8d8f0d89a4be8ef805ca54911272
Nolwandle <ntulinolwandle9@gmail.com>
2022-09-26 16:59:31 -0700

    copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies a memory area
+ * @dest: memory area to be copied
+ * @src: memory area to be copied from
+ * @n: num of bytes to be copied
+ *
+ * Return: pointer to memory block
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int n = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+even more pointers, arrays and strings
